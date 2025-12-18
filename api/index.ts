8ee4d3b2d1b31cf5148bbeac251602df22a52014
Danilo Pereira <128096108+dan1loppl@users.noreply.github.com>
2025-12-18 13:30:31 -0300

    fix: simplify vercel config for Express

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,5 @@
 import { createApp } from '../src/app';
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
 const app = createApp();
 
-// Vercel Serverless Function Handler
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
-}
+export default app;
